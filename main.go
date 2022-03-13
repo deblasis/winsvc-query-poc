6e4666b49f7e751589555395e71ecae54c0dadb9
Alessandro De Blasis <alex@deblasis.net>
2022-03-13 18:40:30 +0000

    least privilege implementation
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- main.go
+++ main.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"log"
+	"syscall"
 
+	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/svc/mgr"
 )
 
 func main() {
 	const svcName = "Dhcp"
 
-	m, err := mgr.Connect()
+	h, err := OpenSCManagerLeastPrivilege()
 	if err != nil {
 		log.Fatalf("failed to connect to Windows service manager: %v", err)
 	}
+	m := &mgr.Mgr{Handle: h}
 	defer m.Disconnect()
 
-	s, err := m.OpenService(svcName)
+	service, err := OpenServiceLeastPrivilege(m, svcName)
 	if err != nil {
 		log.Fatalf("could not access service %v: %v", svcName, err)
 	}
-	defer s.Close()
+	defer service.Close()
 
-	status, err := s.Query()
+	status, err := service.Query()
 	if err != nil {
 		log.Fatalf("could not query service %v: %v", svcName, err)
 	}
 	log.Printf("service %v has status %v", svcName, status)
 
 	return
+}
+
+func OpenSCManagerLeastPrivilege() (windows.Handle, error) {
+	var s *uint16
+	return windows.OpenSCManager(s, nil, windows.SC_MANAGER_CONNECT)
+}
+
+func OpenServiceLeastPrivilege(m *mgr.Mgr, name string) (*mgr.Service, error) {
+	h, err := windows.OpenService(m.Handle, syscall.StringToUTF16Ptr(name), windows.SC_MANAGER_ENUMERATE_SERVICE)
+	if err != nil {
+		return nil, err
+	}
+	return &mgr.Service{Name: name, Handle: h}, nil
 }
